d5c7ea8b3d0a7e9a8d96b063ce2c0012568ce918
atslhamny <atsilahamanyph@gmail.com>
2023-11-02 08:27:17 +0700

    modifikasi foreachloop penonton b3

--- Bioskop04.java
+++ Bioskop04.java
@@ -20,18 +20,24 @@ public class Bioskop04 {
         // System.out.println(penonton[2].length);
         // System.out.println(penonton[3].length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang Baris Ke-" + (i+1) + ": " + penonton[i].length);
-        }
+        // System.out.println(penonton.length);
+        // for (int i = 0; i < penonton.length; i++) {
+        //     System.out.println("Panjang Baris Ke-" + (i+1) + ": " + penonton[i].length);
+        // }
         
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang Baris: " + barisPenonton.length);
+        // for (String[] barisPenonton : penonton) {
+        //     System.out.println("Panjang Baris: " + barisPenonton.length);
+        // }
+
+        System.out.println("Penonton pada baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
         }
         
 
-        System.out.printf("%s \t %s\n",penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n",penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n",penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n",penonton[3][0], penonton[3][1]);
+        // System.out.printf("%s \t %s\n",penonton[0][0], penonton[0][1]);
+        // System.out.printf("%s \t %s\n",penonton[1][0], penonton[1][1]);
+        // System.out.printf("%s \t %s\n",penonton[2][0], penonton[2][1]);
+        // System.out.printf("%s \t %s\n",penonton[3][0], penonton[3][1]);
     }
 }
